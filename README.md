b469ab804916a6b3109e6a1aaa7a8bdf433474de
byteface <byteface@googlemail.com>
2021-10-01 02:04:15 +0100

    create a quick test using htmx.

---
+++ README.md
@@ -0,0 +1,1 @@
+testing domonic with htmx.
---
+++ app.py
@@ -0,0 +1,51 @@
+import json
+
+from sanic import Sanic
+from sanic import response
+
+from domonic.javascript import *
+from domonic.html import *
+from domonic.dom import DocumentFragment
+
+app = Sanic(name='htmxtest')
+
+# create webpage with a socket connection back to our server so it can get the atom data
+page = html(
+    head(
+        DocumentFragment('''
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+        <title>HTMX Test</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>        
+        '''),
+        script(_src="https://unpkg.com/htmx.org@1.5.0")
+        ),
+    body(
+        button("Do something",
+            **{"_data-hx-post":"/clicked"},
+            **{"_data-hx-swap":"outerHTML"}
+        ),
+        button("Do something",
+            **{"_data-hx-post":"/clicked"},
+            **{"_data-hx-swap":"outerHTML"}
+        ),
+        button("Do something",
+            **{"_data-hx-post":"/clicked"},
+            **{"_data-hx-swap":"outerHTML"}
+        ),
+    )
+)
+
+@app.route('/')
+async def home(request):
+  return response.html(f"{page}")
+
+@app.route('/clicked', methods=['POST', 'GET'])
+async def clicked(request):
+  return response.html(f'{div("Button was replaced")}')
+
+app.run(host="0.0.0.0", port=8000)
