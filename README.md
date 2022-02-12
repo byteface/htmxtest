9e27130952eca38db0d677ea584cd1a7a089a24f
mike <mike@eventual.technology>
2022-02-12 11:52:45 +0000

    test htmlx. test windows. found bug in docfrag

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-testing domonic with htmx.
+## htmx test
+
+Just a test repo.
+
+When switching branches make sure to reinstall requirements.txt as i may try different stuff in different branches
+
+
+# intall - linux
+
+```
+cd /Desktop/projects/htmxtest
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+```
+
+
+# intall - windows
+
+```
+cd \Users\bytef\Desktop\projects\package
+python3 -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-import json
-
 from sanic import Sanic
 from sanic import response
 
-from domonic.javascript import *
-from domonic.html import *
-from domonic.dom import DocumentFragment
+from htmlx.tags import html, head, body, script, button, div
+from htmlx.dom import DocumentFragment
 
-app = Sanic(name='htmxtest')
+app = Sanic(name="htmxtest")
 
-# create webpage with a socket connection back to our server so it can get the atom data
 page = html(
     head(
-        DocumentFragment('''
+        # DocumentFragment(
+            """
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
@@ -21,31 +18,38 @@ page = html(
         <title>HTMX Test</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>        
-        '''),
-        script(_src="https://unpkg.com/htmx.org@1.5.0")
-        ),
+        """,
+        # ),
+        script(_src="https://unpkg.com/htmx.org@1.5.0"),
+    ),
     body(
-        button("Do something",
-            **{"_data-hx-post":"/clicked"},
-            **{"_data-hx-swap":"outerHTML"}
+        button(
+            "Do something",
+            **{"_data-hx-post": "/clicked"},
+            **{"_data-hx-swap": "outerHTML"},
         ),
-        button("Do something",
-            **{"_data-hx-post":"/clicked"},
-            **{"_data-hx-swap":"outerHTML"}
+        button(
+            "Do something",
+            **{"_data-hx-post": "/clicked"},
+            **{"_data-hx-swap": "outerHTML"},
         ),
-        button("Do something",
-            **{"_data-hx-post":"/clicked"},
-            **{"_data-hx-swap":"outerHTML"}
+        button(
+            "Do something",
+            **{"_data-hx-post": "/clicked"},
+            **{"_data-hx-swap": "outerHTML"},
         ),
-    )
+    ),
 )
 
-@app.route('/')
-async def home(request):
-  return response.html(f"{page}")
 
-@app.route('/clicked', methods=['POST', 'GET'])
+@app.route("/")
+async def home(request):
+    return response.html(f"{page}")
+
+
+@app.route("/clicked", methods=["POST", "GET"])
 async def clicked(request):
-  return response.html(f'{div("Button was replaced")}')
+    return response.html(f'{div("Button was replaced")}')
+
 
 app.run(host="0.0.0.0", port=8000)
